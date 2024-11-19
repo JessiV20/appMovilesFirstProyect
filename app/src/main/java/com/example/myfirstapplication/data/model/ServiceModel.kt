baaac6192d1c6fce132e954091456fcc4ab9ec17
JessiV20 <jessivilla657@gmail.com>
2024-11-18 18:17:31 -0600

    Merge de Api echas en clase

---
+++ app/src/main/java/com/example/myfirstapplication/data/model/ServiceModel.kt
@@ -0,0 +1,9 @@
+package com.example.myfirstapplication.data.model
+data class ServiceModel(
+    var id: Int = 0,
+    var name: String = "",
+    var username: String = "",
+    var password: String = "",
+    var description: String = "",
+    var imageURL: String? = null
+)
--- app/src/main/java/com/example/myfirstapplication/ui/screens/MenuScreen.kt
+++ app/src/main/java/com/example/myfirstapplication/ui/screens/MenuScreen.kt
@@ -1,14 +1,17 @@
 package com.example.myfirstapplication.ui.screens
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.composable
 
 @Composable
 fun MenuScreen(navController: NavController) {
@@ -19,8 +22,52 @@ fun MenuScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "This is the Menu Screen")
-        Button(onClick = { navController.navigate("components") }) {
+        Button(onClick = { navController.navigate("component") }) {
             Text("Go to Home")
+
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "ApiSegundoPlano 1")
+        Button(onClick = { navController.navigate("SegundoPlano") }) {
+            Text("Go to alarm")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "ApiGeolocalizacion")
+        Button(onClick = { navController.navigate("Location") }) {
+            Text("Go to location")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "ApiCalendario")
+        Button(onClick = { navController.navigate("ContactCalendar") }) {
+            Text("Contact and Calendar")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "ApiBiometricos")
+        Button(onClick = {navController.navigate("BiometricsScreen")}) {
+            Text("Go to Biometrics")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "ApiCamaraYArchivos")
+        Button(onClick = { navController.navigate("CamaraScreen") }) {
+            Text("Camera and files")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "ApiWifi")
+        Button(onClick = {navController.navigate("wifidatos")}) {
+            Text("Go to Wifi-Datos")
+        }
+
     }
 }
